e6d2a1c31264977739b138480ae85ca3d4af0e74
david <d8087869@gmail.com>
2026-06-28 14:29:51 +0300

    1-7

---
+++ toolkit.py
@@ -0,0 +1,45 @@
+# part 1
+#1 
+'''
+def greet(name):
+    print('hello' ,name )
+greet('agent x ')    
+#2 
+def add(a, b):
+    print(a+b)
+add(3 , 4) 
+#3 
+def square(n):
+    print(n ** 2) 
+square(5)
+square(12)  
+#4 
+def greet_with_title(name, title='agent'):
+    print('hello',title,name  ) 
+greet_with_title('david')
+greet_with_title('david', 'ceo')  
+#5 
+def describe(name, level, active):
+    print('name: ' , name)
+    print('level: ' , level)
+    print('active :' , active)
+describe(level= 7 , name= 'david' , active= True) 
+#6 
+def multiply(a, b=2):
+    print(a*b)
+multiply(7)
+multiply(9,9)   
+'''
+#7
+def print_largest(a, b, c):
+    if a >= b :
+        if a>=c :
+            print (a) 
+    elif c>=b :
+        print(c)  
+    else:
+        print(b)  
+print_largest(3,8,5)    
+print_largest(10,2,7)
+print_largest(4,4,1)
+#8 
