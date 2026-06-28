ca354bc058fe6ba4de75fc8245834ed11dbfd324
david <d8087869@gmail.com>
2026-06-28 14:36:00 +0300

    1-7

--- toolkit.py
+++ toolkit.py
@@ -43,3 +43,8 @@ print_largest(3,8,5)
 print_largest(10,2,7)
 print_largest(4,4,1)
 #8 
+def show_fahrenheit(c):
+    print((c * 9 / 5) + 32)
+show_fahrenheit(0)
+show_fahrenheit(100)
+show_fahrenheit(37.5)    
