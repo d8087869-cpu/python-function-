75a9fc23ddabc8d7d13cd972b219cdff8875fc38
david <d8087869@gmail.com>
2026-06-28 15:38:36 +0300

    1-5

--- toolkit.py
+++ toolkit.py
@@ -31,6 +31,7 @@ multiply(7)
 multiply(9,9)   
 '''
 #7
+'''
 def print_largest(a, b, c):
     if a >= b :
         if a>=c :
@@ -69,3 +70,16 @@ def power(base, exponent=2):
 power(3)
 power(3,3)
 power(exponent=4 , base=2)    
+'''
+#4 
+def repeat(text, times):
+    print(text*times)
+repeat('ha', 3)    
+repeat('ka',2)
+#5 
+def flatten_and_print(matrix):
+    for row in matrix :
+        for cell in row :
+            print(cell)
+x =[[1, 2], [3, 4], [5]]    
+flatten_and_print(x)
