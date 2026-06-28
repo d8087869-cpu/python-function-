02a047792ba9cbe440a29002ad394de84390ac2d
david <d8087869@gmail.com>
2026-06-28 16:35:30 +0300

    1-2

--- toolkit.py
+++ toolkit.py
@@ -63,14 +63,30 @@ def summarize(items):
     print('min' , min(items))
     print('max', max(items))
 summarize([4, 9, 2, 10, 3])    
-#part 2
+#part 2'''
+
+#1
+def show_all(*args):
+    print(args)
+show_all('radio', 'map' , 'flashligth')   
+
+
+#2 
+def show_profile(**kwargs):
+    print(kwargs)
+
+show_profile(name="Agent X", level=7, active=True)
+
+
+
+'''
 #3
 def power(base, exponent=2):
     print(base**exponent)
 power(3)
 power(3,3)
-power(exponent=4 , base=2)    
-'''
+power(exponent=4 , base=2)
+
 #4 
 def repeat(text, times):
     print(text*times)
@@ -83,3 +99,4 @@ def flatten_and_print(matrix):
             print(cell)
 x =[[1, 2], [3, 4], [5]]    
 flatten_and_print(x)
+'''
