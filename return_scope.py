08062856da3e645908e7f95307e208871b3d509a
david <d8087869@gmail.com>
2026-06-29 11:26:11 +0300

    2-5

--- return_scope.py
+++ return_scope.py
@@ -1,29 +1,26 @@
 #1
-# 8 becuse in the scope you added a score and it did not get out to the global 
-# 10 becuse you printed only from the global . 
+# 8 -becuse in the scope you added a score and it did not get out to the global 
+# 10 -becuse you printed only from the global . 
+
 #2
-name = "Agent" 
+#spy - becouse the func was activeted and printed 
+#40  - also was activeted by the function and printed 
+#agent - printed from the global and nothing to do with the function
+# 2 - level in the global was printed . and level in the scope was not active
 
-level = 2 
+#3 
+#30 - the function takes the val of coins and changes the val to coins 20 =+10 =30
+# 20 after that coins is worth 10 its multiply by 2 = 20 and you activeted the func and the coines that are inside . 
 
- 
+#4
+#  
+#70 - in the global its 100 / in func we made a new val helth= helth-damge, and activated the fun with 30 uts 100-30=70 
+#35 - made a new val in the fun +5 /30+5 and 70 -35 = 35 
+#100 - printed the global val 
 
-def show_info(): 
+#5 
+#['map', 'key', 'torch', 'coin']- in the scope was added 2 new val in the list so when active the func you will see all new info 
+#['map', 'key', 'torch', 'coin'] - when printed the items from global it points all the val that are exciting and add in to the list
 
-    name = "Spy" 
-
-    level = 4 
-
-    power = level * 10 
-
-    print(name) 
-
-    print(power) 
-
- 
-
-show_info() 
-
-print(name) 
-
-print(level) 
+#6 
+#
