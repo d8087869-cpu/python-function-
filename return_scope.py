0f9dc00c8f6e2104ac4853e656abfe21d31e9bd9
david <d8087869@gmail.com>
2026-06-29 11:24:18 +0300

    2-5

--- return_scope.py
+++ return_scope.py
@@ -1,3 +1,29 @@
 #1
 # 8 becuse in the scope you added a score and it did not get out to the global 
 # 10 becuse you printed only from the global . 
+#2
+name = "Agent" 
+
+level = 2 
+
+ 
+
+def show_info(): 
+
+    name = "Spy" 
+
+    level = 4 
+
+    power = level * 10 
+
+    print(name) 
+
+    print(power) 
+
+ 
+
+show_info() 
+
+print(name) 
+
+print(level) 
